e3a8cafd2b0192eb794101f6920173b174edd943
guillegalor <guillegalor@gmail.com>
2019-01-15 11:05:22 +0100

    Añadidos hpp que faltaban

---
+++ practica4.hpp
@@ -0,0 +1,17 @@
+// *********************************************************************
+// **
+// ** Informática Gráfica
+// ** Práctica 4  (declaraciones públicas)
+// **
+// *********************************************************************
+
+#ifndef IG_PRACTICA4_HPP
+#define IG_PRACTICA4_HPP
+
+#include "materiales.hpp"
+
+void P4_Inicializar(  ) ;
+bool P4_FGE_PulsarTeclaCaracter(  unsigned char tecla ) ;
+void P4_DibujarObjetos( ContextoVis & cv ) ;
+
+#endif
